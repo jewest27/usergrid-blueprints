938dc785366e5a72d34b46f3054834a5c5540d1f
ayesha12 <ayesha.amrin@gmail.com>
2015-07-09 17:19:25 -0700

    adding tests

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,7 +1,4 @@
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Graph;
-import com.tinkerpop.blueprints.GraphFactory;
-import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.*;
 import org.apache.usergrid.drivers.blueprints.EntityId;
 import org.apache.usergrid.drivers.blueprints.UsergridEdge;
 //import org.apache.usergrid.java.client.model.EntityId;
@@ -20,27 +17,53 @@ public class TestApp {
 
     Graph usergrid = GraphFactory.open("/Users/ayeshadastagiri/usergrid-blueprints/src/main/resources/usergrid.properties");
 
-    Vertex v1 = usergrid.addVertex("person:ayesha");
-    System.out.println("id v1 :: " + usergrid.getVertex(v1.getId()));
+    System.out.println("Creating VERTICES");
+    Vertex person1 = usergrid.addVertex("person:ayesha");
+    System.out.println("id person1 :: " + person1.getId());
+
+    Vertex restaurant1 = usergrid.addVertex("restaurant:amici");
+    System.out.println("id restaurant1 :: " + restaurant1.getId());
+
+    Vertex restaurant2 = usergrid.addVertex("restaurant:CPK");
+    System.out.println("id restaurant2 :: " + restaurant2.getId());
+
+    System.out.println();
+    System.out.println("Getting VERTICES");
+    Vertex testGet = usergrid.getVertex(person1.getId()); //Gets vertex using getVetex which in turn uses getId
+    System.out.println(testGet);
+
+    System.out.println();
+    System.out.println("Setting and getting propertices for VERTICES");
+    restaurant1.setProperty("tag", "Italian"); // Sets a property
+    System.out.println("Getting the property for Amici restaurant : " + restaurant1.getProperty("tag")); //Gets a property
+
+    System.out.println();
+    System.out.println("Adding EDGES");
+    Edge e1 = person1.addEdge("visits",restaurant1);
+    System.out.println("Edge1 id : " + e1.getId());
+
+    Edge e2 = usergrid.addEdge(null,person1,restaurant2,"visits");
+    System.out.println("Edge2 id : "  + e2.getId());
+
+    Edge e3 = person1.addEdge("likes",restaurant1);
+    System.out.println("Edge3 id : " + e3.getId());
 
 
-    Vertex v2 = usergrid.addVertex("restaurant:amici");
-    System.out.println("id v2 :: " +usergrid.getVertex(v2.getId()));
+    System.out.println();
+    System.out.println("Getting EDGES");
+    String edgeId = person1.getId()+"-->visits-->"+restaurant1.getId();
+    e3 = usergrid.getEdge(edgeId);
+    System.out.println("Get edge : " + e3.getId());
 
 
-    //Object eId = "type:visits";
-    Edge e1 = usergrid.addEdge(null,v1,v2,"visits");
-    //e1.getId();
-    String edgeId = v1.getId()+"-->Visits";
-
-    Edge e2 = usergrid.getEdge(edgeId);
-//    System.out.println(e2.getId());
-//    Vertex v2 = usergrid.getVertex("<type>:<uuid>");
+//    System.out.println("Deleting EDGES");
 //
-//    Vertex v2 = usergrid.getVertex(new EntityId("type", UUID.fromString("uuid")));
+//    usergrid.removeEdge(e3);
+//    e2.remove();
 //
-    //EntityId test = new EntityId("people:ab711e95-2432-11e5-8a7e-8e25b400a634");
-    //System.out.println(usergrid.getVertex(test));
-//    System.out.println(usergrid.getVertex("people:ab711e95-2432-11e5-8a7e-8e25b400a634"));
+//    System.out.println("Deleting VERTICES");
+//    usergrid.removeVertex(restaurant1);
+//    restaurant2.remove();
+
   }
 }
